c5fa3b5be87d5e58cb91a4da2bf07a59216c12fc
saihe <saihe.kyon@gmail.com>
2015-08-17 18:14:33 +0900

    Battole

---
+++ Battole/Assets/Battle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battle : MonoBehaviour {
+	public int stamina;
+
+	// Use this for initialization
+	void Start () {
+		stamina = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			stamina -= 1;
+		}
+		if (stamina == 0) {
+			Application.LoadLevel(Application.loadedLevel);
+		}
+		print (stamina);
+	}
+}
