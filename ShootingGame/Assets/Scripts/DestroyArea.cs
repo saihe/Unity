ed792a2becff6a8e09a13af0fd9c2e6527a34735
saihe <saihe.kyon@gmail.com>
2015-10-03 15:31:03 +0900

    Waveまで作り終えた。

---
+++ ShootingGame/Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyArea : MonoBehaviour {
+
+    void OnTriggerExit2d(Collider2D c)
+    {
+        Destroy(c.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ShootingGame/Assets/Scripts/Emitter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Emitter : MonoBehaviour {
+
+    //Waveプレハブを格納する
+    public GameObject[] waves;
+
+    //現在のWave
+    private int currentWave;
+
+	// Use this for initialization
+	IEnumerator Start () {
+	
+        //Waveが存在しなければコルーチンを終了する
+        if(waves.Length == 0){
+            yield break;
+        }
+        while (true)
+        {
+            //Waveを作成する
+            GameObject wave = (GameObject)Instantiate(waves[currentWave], transform.position, Quaternion.identity);
+
+            //Waveの子要素のEnemyがすべて削除されるまで待機する
+            while(wave.transform.childCount != 0)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            //Waveの削除
+            Destroy(wave);
+
+            //格納されているWaveをすべて実行したらcurrentWaveを0にする(最初から -> ループ)
+            if(waves.Length <= ++currentWave)
+            {
+                currentWave = 0;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
