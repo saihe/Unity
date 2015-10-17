b03a6f59e8ea64a29aeb658e5a4acda5757ca686
saihe <saihe.kyon@gmail.com>
2015-10-17 09:51:53 +0900

    UnityShootingをPC/WebPlayer/Android用にビルドした。 Enemyをアクティブに復帰すると位置情報とかがばらばらだし、三周目からはコルーチンのMoveが動かない、弾は撃ってる。

---
+++ Test.1016_Q2/Assets/Scripts/summon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class summon : MonoBehaviour {
+
+    public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnGUI()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(sphere, sphere.transform.position, sphere.transform.rotation);
+        }
+    }
+}
